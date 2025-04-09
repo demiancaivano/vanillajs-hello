e8a6649a1ce825b2eab820c0314475ddec3e6e5e
Demian <demian.caivano@gmail.com>
2025-04-09 11:38:39 +0000

    Generador excusas final

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 import "bootstrap";
 import "./style.css";
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let encabezado = "No me lo vas a creer, pero...  ";
+  let quien = ["Un mono de 3 cabezas ", "Un cocodrilo ", "Un pájaro carpintero ", "Mi tía "];
+  let quehizo = ["se comió ", "destruyó ", "arruinó ", "hizo desaparecer "];
+  let quecosa = ["mi premio Nobel ", "la vajilla nueva ", "mi souvenir de Vietnam ", "el último alfajor "];
+  let cuando = ["ayer. ", "hace 2 semanas ", "el verano pasado. ", "hace una hora. "];
+  
+  let rdm1 = Math.floor(Math.random() * quien.length);
+  let rdm2 = Math.floor(Math.random() * quehizo.length);
+  let rdm3 = Math.floor(Math.random() * quecosa.length);
+  let rdm4 = Math.floor(Math.random() * cuando.length);
+
+  document.querySelector("#excuse").innerHTML = encabezado + quien[rdm1] + quehizo[rdm2] + quecosa[rdm3] + cuando[rdm4];
+  
 };
